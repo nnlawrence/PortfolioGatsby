43b46836239a96ef220247c35410de1f34218045
Nathaniel Lawrence <nathanielawrence@gmail.com>
2019-12-13 00:49:32 -0700

    portfolio

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,15 @@ import React from 'react'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import pic04 from '../images/IMG_2660.jpg'
+import pic05 from '../images/IMG_puma.jpeg'
+import pic06 from '../images/html5.png'
+import pic07 from '../images/css3.png'
+import pic08 from '../images/react.png'
+import pic09 from '../images/node.png'
+import pic10 from '../images/express.png'
+import pic11 from '../images/js.png'
+import pic12 from '../images/postgresql.png'
 
 class Main extends React.Component {
   render() {
@@ -28,30 +37,13 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Intro</h2>
+          <h2 className="major">Nathaniel Lawrence</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={pic04} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
-          </p>
+            My name is Nathaniel Lawrence (on the right), I reside in Utah, but I've lived all over and love to travel. I'm always open for a new adventure. I pride myself on developing and designing. Creation is a passion of mine. A lot of the things we now use every day haven't been available for a long period of time. As a developer, the world is a playground and open to explore. Other passions of mine include family and sports; basketball, football, soccer, rugby, etc. I can spend hours talking about just that. I like to inspire others and encourage them to follow their dreams. Just a genuine guy who likes to see the progress and success of others, make a difference, and have fun.</p>
+            <p>Check out my <a href="https://github.com/nnlawrence"> work on GitHub</a>.</p>
           {close}
         </article>
 
@@ -62,25 +54,17 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Work</h2>
+          <h2 className="major">Projects</h2>
           <span className="image main">
-            <img src={pic02} alt="" />
+            <h3>TechPanda</h3>
+          <iframe src="https://player.vimeo.com/video/377590207" width="100%" height="400" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <p><a href="https://vimeo.com/377590207">TechPanda</a></p>
+          <p>Group full-stack eCommerce web application that allows users to view and purchase new tech from google and apple. I implemented all animations, HTML, CSS, etc.</p>
+          <h3>Voilà</h3>
+          <iframe src="https://player.vimeo.com/video/377698079" width="100%" height="321" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <p><a href="https://vimeo.com/377698079">Voila</a></p>
           </span>
-          <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
-          </p>
+          <p>Full-stack web application that allows users to locate food trucks and view information about them. Food truck owners can add new or additional food trucks and update their coordinates to be shown in a new location. RESTful API design</p>
           {close}
         </article>
 
@@ -91,19 +75,28 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">About</h2>
+          <h2 className="major">Skills</h2>
           <span className="image main">
-            <img src={pic03} alt="" />
+            <img src='https://www.ionos.com/digitalguide/fileadmin/DigitalGuide/Teaser/code-editoren-t.jpg' width='300' alt="" />
           </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
-          </p>
+          <img src='https://static.parastorage.com/client/pfavico.ico' />
+          <div className='skillio'>
+            <h3>HTML, CSS, React, Node, Express, Javascript, Postgresql, Material-UI, Git, Sketch, etc.</h3>
+            <div>
+            <img src={pic06} width='94' margin='20%'/>
+            <img src={pic07} width='100' />
+            <img src={pic08} width='100' />
+            <img src={pic09} width='100' />
+            <img src={pic10} width='100' />
+            </div>
+            <div>
+            <img src={pic11} width='95' />
+            <img src={pic12} width='100' />
+            <img src='https://static.wixstatic.com/media/b4bcde_22e16bf1307142868330102c0f3553ea~mv2.png/v1/fill/w_324,h_256,al_c,q_80,usm_0.66_1.00_0.01/material.webp' width='100' />
+            <img src='https://static.wixstatic.com/media/b4bcde_e69c9f647bb848449b5c7e1019efe3de~mv2.png/v1/fill/w_284,h_282,al_c,q_80,usm_0.66_1.00_0.01/Git-Icon-1788C.webp' width='100' />
+            <img src='https://www.sketch.com/images/pages/press/sketch-press-kit/app-icons/sketch-mac-icon@2x.png' width='100' />
+            </div>
+          </div>
           {close}
         </article>
 
@@ -115,14 +108,14 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" action="https://formspree.io/nathanielawrence@gmail.com">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="text" name="_replyto" id="email" />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
@@ -140,28 +133,36 @@ class Main extends React.Component {
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/nategottheflu"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
+              <a href="https://www.facebook.com/nathaniel.lawrence.7" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
+              <a href="https://www.instagram.com/sirnaynay/" className="icon fa-instagram">
                 <span className="label">Instagram</span>
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://github.com/nnlawrence"
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/nnlawrence/"
+                className="icon fa-linkedin"
+              >
+                <span className="label">Linkedin</span>
               </a>
             </li>
           </ul>
